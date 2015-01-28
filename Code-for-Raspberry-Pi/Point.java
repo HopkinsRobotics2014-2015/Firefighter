8e5a4ecfb2284f0f782ec8d142c2e2c17d8cc2f7
Matt <hpotter134@gmail.com>
2015-01-28 14:18:49 -0500

    Create Point.java
    Useful to have, though not absolutely necessary. In future may want to make this an inner class of Maps.java

---
+++ Code-for-Raspberry-Pi/Point.java
@@ -0,0 +1,11 @@
+public class Point{
+    int x;
+    int y;
+    public Point(int _x, int _y){
+        this.x = _x;
+        this.y = _y;
+    }
+    public String toString(){
+        return ("(" + x + ", " + y + ")");
+    }
+}
