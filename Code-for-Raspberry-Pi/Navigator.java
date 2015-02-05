6db2d8b09567c5ae77cb72be1ab1401a208ef3fb
Matt <hpotter134@gmail.com>
2015-02-05 10:42:49 -0500

    Added ASTAR code
    hooray for algorithms.
    
    resolves #11

--- Code-for-Raspberry-Pi/Navigator.java
+++ Code-for-Raspberry-Pi/Navigator.java
@@ -3,9 +3,72 @@ class Navigator{
         
     }
     
+    private class Node {
+        int x,y;
+        float f,g,h;
+        Node next;
+        ArrayList<Node> neighbors;
+        public Node(int _x, int _y){
+            this.x = _x;
+            this.y = _y;
+            this.f = Float.POSITIVE_INFINITY;
+            this.g = 0;
+            this.h = 0;
+            this.neighbors = new ArrayList<Node>();
+        }
+        
+    }
+    
     getPath(start, end, map)?
     
-    
+    void ASTAR(Node startNode, Node endNode){
+      ArrayList<Node> open = new ArrayList<Node>();
+      ArrayList<Node> closed = new ArrayList<Node>();
+      
+      boolean foundPath = false;
+      
+      Node q = endNode;
+      q.f = 0;
+      q.g = 0;
+      
+      open.add(q);
+      
+      while (!open.isEmpty()){
+        
+        float minf = Float.POSITIVE_INFINITY;
+        for (Node n : open){
+          if (n.f < minf){
+            q = n;
+            minf = n.f;
+          }
+        }
+        
+        if (q == startNode){
+          foundPath = true;
+          break;
+        }
+        
+        open.remove(q);
+        
+        for (Node neighbor : q.neighbors){
+          float g = q.g + q.getCost(neighbor);
+          float h = neighbor.getStraightLineDistance(startNode);
+          float f = g + h;
+          
+          if (f < neighbor.f){
+            neighbor.f = f;
+            neighbor.g = g;
+            neighbor.next = q;
+          }
+          
+          if (!open.contains(neighbor) && !closed.contains(neighbor)){
+            open.add(neighbor);
+          }
+      
+        }
+        
+        closed.add(q);
+      }
     
 }
 
