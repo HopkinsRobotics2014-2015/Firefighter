82cfc9c6349ee006659a52fbfce34bcd8be59b39
mattvanmierlo <mattvanmierlo@gmail.com>
2015-02-13 14:59:25 -0500

    Update Fan and Ultrasonic Sensor Code.ino

--- Code-for-Arduino/Fan and Ultrasonic Sensor Code.ino
+++ Code-for-Arduino/Fan and Ultrasonic Sensor Code.ino
@@ -4,10 +4,29 @@ int fanPin =
 
 void setup()
 {
-
+  Serial.begin(9600);
+  pinMode(echoOut, OUTPUT);
+  pinMode(echoIn, INPUT);
+  pinMode(fanPin, OUTPUT);
 }
 
 void loop()
 {
-
+  digitalWrite(echoOut, LOW);
+  delay(2);
+  digitalWrite(echoOut, HIGH);
+  delay(10);
+  digitalWrite(echoOut, LOW);
+  int distance = pulseIn(inputPin, HIGH);
+  float distance1 = distance/57.355; //cm
+  Serial.println(distance1);
+  delay(100);
+  if(distance1 < 10)
+  {
+    digitalWrite(fanPin,HIGH); 
+  }
+  else
+  {
+    digitalWrite(fanPin, LOW);
+  }
 }
